152c97e01fcbb5edf86ec6db71a41fb3d5e396bb
prapti260108 <praptipatel260108@gmail.com>
2025-02-27 12:35:26 +0530

    Update userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { signup,login } = require('../controller/userController');
-const AuthMiddleware = require("../Middleware/AuthMiddleware");
+const {authenticate} = require("../Middleware/AuthMiddleware");
 const router = express.Router();
 
 
 // User signup
-router.post('/signup',AuthMiddleware, signup);
+router.post('/signup',authenticate, signup);
 // User login
 router.post('/login', login);
 
